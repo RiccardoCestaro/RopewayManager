ceb4c3537ebc1af5675f6bfa1f182ddcafa34659
Riccardo Cestaro <riccardo.cestaro@outlook.it>
2019-05-28 16:35:11 +0200

    moving into Model folder

--- invalidstringtoenumexception.cpp
+++
@@ -1,5 +0,0 @@
-#include "invalidstringtoenumexception.h"
-
-InvalidStringToEnumException::InvalidStringToEnumException() : error("Invalid String. Unable to assign to enum value"){
-    std::cout << error <<std::endl;
-}
